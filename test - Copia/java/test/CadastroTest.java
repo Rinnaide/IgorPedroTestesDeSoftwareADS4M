1a42d651802b932bb54b14312ef0c7b796a0eaa7
DigiDestinedCode <igorismel@gmail.com>
2025-12-05 12:17:39 -0300

    Refactor CadastroTest with additional test cases

--- test - Copia/java/test/CadastroTest.java
+++ test - Copia/java/test/CadastroTest.java
@@ -19,6 +19,16 @@ public class CadastroTest extends BaseTest {
 		loginPage = new LoginPO(driver);
 	}
 
+
+	
+	@Test
+	public void TC010_deveVerificarCadastroDisponivelAposLogin() {
+		loginPage.executarAcaoDeLogar("teste", "teste");
+
+		assertTrue(cadastroPage.buttonCadastro.isDisplayed());
+	}
+
+	
 	@Test
 	public void TC011_naoDeveCadastrarProdutoNoSistemaComCodigoNomeQuantidadeValorEDataVazios() {
 		cadastroPage.CaixaCadastro("", "","","","");
@@ -28,11 +38,50 @@ public class CadastroTest extends BaseTest {
 		assertEquals(mensagem, "Todos os campos são obrigatórios para o cadastro!");
 	}
 
+		
 	@Test
-	public void TC012_deveVerificarCadastroDisponivelAposLogin() {
-		loginPage.executarAcaoDeLogar("admin@admin.com", "admin@123");
-
-		assertTrue(cadastroPage.buttonCadastro.isDisplayed());
+	public void TC012_naoDeveCadastrarProdutoNoSistemaComCodigoNomeQuantidadeValorEDataVazios() {
+		cadastroPage.CaixaCadastro("", "teste","teste","teste","teste");
+		
+		String mensagem = cadastroPage.obterMensagemCadastro();
+		
+		assertEquals(mensagem, "Todos os campos são obrigatórios para o cadastro!");
+	}
+	
+	@Test
+	public void TC013_naoDeveCadastrarProdutoNoSistemaComCodigoNomeQuantidadeValorEDataVazios() {
+		cadastroPage.CaixaCadastro("teste", "","teste","teste","teste");
+		
+		String mensagem = cadastroPage.obterMensagemCadastro();
+		
+		assertEquals(mensagem, "Todos os campos são obrigatórios para o cadastro!");
+	}
+	
+	@Test
+	public void TC014_naoDeveCadastrarProdutoNoSistemaComCodigoNomeQuantidadeValorEDataVazios() {
+		cadastroPage.CaixaCadastro("teste", "teste","","teste","teste");
+		
+		String mensagem = cadastroPage.obterMensagemCadastro();
+		
+		assertEquals(mensagem, "Todos os campos são obrigatórios para o cadastro!");
+	}
+	
+	@Test
+	public void TC015_naoDeveCadastrarProdutoNoSistemaComCodigoNomeQuantidadeValorEDataVazios() {
+		cadastroPage.CaixaCadastro("teste", "teste","teste","","teste");
+		
+		String mensagem = cadastroPage.obterMensagemCadastro();
+		
+		assertEquals(mensagem, "Todos os campos são obrigatórios para o cadastro!");
+	}
+	@Test
+	public void TC016_naoDeveCadastrarProdutoNoSistemaComCodigoNomeQuantidadeValorEDataVazios() {
+		cadastroPage.CaixaCadastro("teste", "teste","teste","teste","");
+		
+		String mensagem = cadastroPage.obterMensagemCadastro();
+		
+		assertEquals(mensagem, "Todos os campos são obrigatórios para o cadastro!");
 	}
 
 }
+
