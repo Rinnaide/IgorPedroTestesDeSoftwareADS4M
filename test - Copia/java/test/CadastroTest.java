0b4c1bb05047636f9a50069718e4f0fa2b2e9cf1
Pedro Costa <phencostarj@gmail.com>
2025-12-05 12:15:02 -0300

    Refactor CadastroTest to include LoginPO and new test

--- test - Copia/java/test/CadastroTest.java
+++ test - Copia/java/test/CadastroTest.java
@@ -1,28 +1,38 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import pageObject.CadastroPO;
+import pageObject.LoginPO;
 
-public class CadastroTest extends BaseTest2 {
+public class CadastroTest extends BaseTest {
 	public static CadastroPO cadastroPage;
+	public static LoginPO loginPage;
 
 	@BeforeClass
 	public static void prepararTestes() {
 		cadastroPage = new CadastroPO(driver);
+		loginPage = new LoginPO(driver);
 	}
 
 	@Test
 	public void TC011_naoDeveCadastrarProdutoNoSistemaComCodigoNomeQuantidadeValorEDataVazios() {
 		cadastroPage.CaixaCadastro("", "","","","");
-		
+
 		String mensagem = cadastroPage.obterMensagemCadastro();
-		
+
 		assertEquals(mensagem, "Todos os campos são obrigatórios para o cadastro!");
 	}
-	
-}
 
+	@Test
+	public void TC012_deveVerificarCadastroDisponivelAposLogin() {
+		loginPage.executarAcaoDeLogar("admin@admin.com", "admin@123");
+
+		assertTrue(cadastroPage.buttonCadastro.isDisplayed());
+	}
+
+}
